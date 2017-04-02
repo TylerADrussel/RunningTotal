d5c6efb063af3c6b6766b9e79c3e00ae5bcfa4d9
Tyler Drussel <Zuko@tylers-macbook-pro.local>
2017-04-02 11:20:03 -0600

    Created storyboard with header view, linked outlets to top views.

---
+++ Running Total/BucketCell.swift
@@ -0,0 +1,15 @@
+//
+//  BucketCell.swift
+//  Running Total
+//
+//  Created by Tyler Drussel on 4/2/17.
+//  Copyright © 2017 Tyler Drussel. All rights reserved.
+//
+
+import UIKit
+
+class BucketCell: FoldingCell {
+    
+    
+    
+}
